a4aa1492b28b739bf09cdb43a1567dddcee3e9cb
WillowPunch <danek2002@gmail.com>
2022-12-05 16:36:29 +0300

    add evthg
    add erin

---
+++ public_html/delete.php
@@ -0,0 +1,45 @@
+<?php
+include 'functions.php';
+// Delete Controller Part
+$pdo = pdo_connect_mysql();
+$msg = '';
+if (isset($_GET['id'])) {
+    $stmt = $pdo->prepare('SELECT * FROM polls WHERE id = ?');
+    $stmt->execute([ $_GET['id'] ]);
+    $poll = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$poll) {
+        exit('Poll doesn\'t exist with that ID!');
+    }
+    if (isset($_GET['confirm'])) {
+        if ($_GET['confirm'] == 'yes') {
+            $stmt = $pdo->prepare('DELETE FROM polls WHERE id = ?');
+            $stmt->execute([ $_GET['id'] ]);
+            $stmt = $pdo->prepare('DELETE FROM poll_answers WHERE poll_id = ?');
+            $stmt->execute([ $_GET['id'] ]);
+            $msg = 'You have deleted the poll!';
+        } else {
+            header('Location: index.php');
+            exit;
+        }
+    }
+} else {
+    exit('No ID specified!');
+}
+?><?php echo template_header('Delete'); ?>
+
+<!-- Delete View Part -->
+
+<div class="content delete">
+	<h2>Delete Poll #<?=$poll['id']?></h2>
+    <?php if ($msg): ?>
+    <p><?=$msg?></p>
+    <?php else: ?>
+	<p>Are you sure you want to delete poll #<?=$poll['id']?>?</p>
+    <div class="yesno">
+        <a href="delete.php?id=<?=$poll['id']?>&confirm=yes">Yes</a>
+        <a href="delete.php?id=<?=$poll['id']?>&confirm=no">No</a>
+    </div>
+    <?php endif; ?>
+</div>
+
+<?php echo template_footer(); ?>
